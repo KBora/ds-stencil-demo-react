a9080943728e66a6652fc4c602234bccea765948
wookkii <wookkii@home>
2021-05-12 22:33:30 +1000

    Add card wrapper

---
+++ src/Card.js
@@ -0,0 +1,28 @@
+import React, { useRef, useEffect} from "react";
+
+const Card = (props) => {
+  const elementRef = useRef(null);
+
+  useEffect( () => {
+    // assign non scalar props
+    elementRef.current.cardTags = props.cardTags;
+
+    // listen for custom events
+    elementRef.current.addEventListener('tagClick', $event => {
+      props.tagClick($event);
+    });
+
+  }, [props]);
+
+  return (
+    <ds-card
+      card-image-url={props.cardImageUrl}
+      card-image-alt={props.cardImageAlt}
+      card-title={props.cardTitle}
+      card-text={props.cardText}
+      ref={elementRef}>
+    </ds-card>
+  )
+}
+
+export default Card;
